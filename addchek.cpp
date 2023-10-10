c03e82ed3c117ac67bbe394f1eeaa0adccc7d187
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-10-10 15:16:00 +0700

    Create addchek.cpp

---
+++ addchek.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+int addition(unsigned long int n){
+int sum = 0;
+
+while(n != 0){
+    sum += n%10;
+    n = n/10;
+
+
+}
+printf("%d\n", sum);
+return sum;
+}
+
+int addchek(unsigned long int n){
+    int cnt = 0;
+while(n/10 != 0){
+    n = addition(n);
+    cnt ++;
+    printf("%d\n", n);
+    printf("%d\n", cnt);
+}
+return cnt;
+}
+
+int main()
+{
+    printf("input number for addition check\n");
+  unsigned long int n;
+  scanf("%d", &n);
+  int res = addchek(n);
+  printf("%d", res);
+
+
+    return 0;
+}
