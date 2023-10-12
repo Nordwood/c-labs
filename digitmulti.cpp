c72b944976b216221477d86c2d05eaf9c0d13fc4
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-10-12 21:01:49 +0700

    Update digitmulti.cpp

--- digitmulti.cpp
+++ digitmulti.cpp
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void test_multi(){
+if (multi(1) == 1){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+if (multi(5) == 5){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+
+if (multi(123) == 6){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+}
 int multi(int n){
 int sum = 1;
-
 while(n != 0){
     sum *= n%10;
     n = n/10;
@@ -14,6 +32,7 @@ return sum;
 
 int main()
 {
+    test_multi();
     printf("input number for multidigit\n");
   int n;
   scanf("%d", &n);
