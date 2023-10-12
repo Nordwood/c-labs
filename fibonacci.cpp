091dc679bc6a2935c942203ca1b3a1147ba680d2
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-10-12 19:41:02 +0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+void test_fibonacci(){
+if (fibonacci(1) == 1){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+if (fibonacci(7) == 13){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+
+if (fibonacci(5) == 5){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+}
 int fibonacci(int n){
 if(n == 1 || n == 2){
     return 1;
@@ -8,6 +26,7 @@ return fibonacci(n - 1) + fibonacci(n - 2);
 }
 int main()
 {
+test_fibonacci();
 printf("input int number for fibonaci\n");
   int n;
   scanf("%d", &n);
