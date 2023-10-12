254fbf6cb70170ea83c3de79ff4b5ed8e097fe50
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-10-12 22:19:50 +0700

    Update power.cpp

--- power.cpp
+++ power.cpp
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+void test_power(){
+if (power(1, 1) == 1){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+if (power(2,3) == 8){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+
+if (power(10,3) == 1000){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+}
 int power(int a, int b){
 int i = 1;
-int s = a;
+int s = 1;
 for(i; i <= b; i++){
         s *= a;
     }
@@ -10,7 +28,8 @@ for(i; i <= b; i++){
 }
 int main()
 {
-    printf("input 2 int numbers\n");
+        test_power();
+    printf("input 2 int numbers for sqr\n");
     int a;
     int b;
     scanf("%d", &a);
