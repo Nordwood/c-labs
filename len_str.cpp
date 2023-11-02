a4692fd6ffd95d1d5466c0ee7694f7489420c737
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-11-02 15:29:34 +0700

    Update len_str.cpp

--- len_str.cpp
+++ len_str.cpp
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void test_lenstr(){
+int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int c[12] = {0, 1, 2, 3, 4, 5, 6, 7, 10, 12, 13,15};
 
+if (len_cst(a, 10)(a, 10) == 10){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+if (len_cst(s, 10)(a, 10) == 12){
+    printf("ok\n");}
+    else{
+        printf("test failed\n");
+            }
+}
+int random_array(int *x, int b){
+    int s;
+    for (int i = 0; i<b; i++){
+    s = rand() % 100;
+    x[i] = s;
+    }
+   return x;
+}
 
 
 
@@ -16,6 +38,7 @@ int len_cstr(char *x, int b){
 int main()
 {
     char str[256] = "";
+    test_lenstr();
     printf("input str: ");
     scanf("%255s",str);
     printf("output: %s",str);
