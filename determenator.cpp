78afc9d1706d5568b26990ca0c4fa666ecaa9076
IzikaevADfl-31 <34055013+Nordwood@users.noreply.github.com>
2023-11-08 23:12:42 +0700

    Update determenator.cpp

--- determenator.cpp
+++ determenator.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 // не работает я устал исправлять и искать ошибки
-void test_determ(){
+/*void test_determ(){
 int a[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 int s[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 11 };
 if (determ_matrx(a, 9) == 0){
@@ -16,32 +16,33 @@ if (determ_matrx(s, 10) == -6){
     else{
         printf("test failed\n");
             }
+}*/
+int calculateDeterm(int **matrix, int size){
+int det = 0;
+if (size == 2)
+ {return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];}
+int **tempMat;
+int sign = 1;
+int i, j, k;
+tempMat = (int **)malloc((size - 1) * sizeof(int *));
+for (i = 0; i < size - 1; i++)
+ {tempMat[i] = (int *)malloc((size - 1) * sizeof(int));}
+for (k = 0; k < size; k++){
+ int temp_i = 0, temp_j = 0;
+for (i = 1; i < size; i++){
+        for (j = 0; j < size; j++){
+          if (j != k){
+               tempMat[temp_i][temp_j++] = matrix[i][j];}
 }
-int determ_matrx(int **a ,int n){
-int **mn = random_create(a, n);
-int **r = random_create(a, n);
-int d;
-if (n == 1){
-    d = a[0][0];}
-else if (n == 2){
-    d = a[0][0]*a[1][1] - a[1][0]*a[0][1];
+temp_i++;
+temp_j = 0;}
+det += sign * matrix[0][k] * calculateDeterm(tempMat, size - 1);
+sign = -sign;}
+for (i = 0; i < size - 1; i++){
+    free(tempMat[i]);
 }
-else {
-    int s;
-    for (int i = 0; i<n; i++){
-       mn[i] = (int*) malloc((i + 1) * sizeof(int));
-        for (int j = 0; j<n; j++){
-           r[j] = (int*) malloc((j + 1) * sizeof(int));
-                for (int k = 0; k<n; k++){
-                    if((j != 0) && (k != i)){
-                       r += a[j][k];
-                       }
-                }mn += r;
-        }s += a[0][i]*determ_matrx(mn, n - 1 )*(-1)**1;
-    }
-   d = s;
-}
-return d;
+free(tempMat);
+return det;
 }
 
 
@@ -74,10 +75,10 @@ void show_matrx(int **a, int n){
 int main()
 {
 
- int i, j, n;
+  int n, i, j, deter;
 
   srand(time(0));
-  void test_determ();
+  //void test_determ();
   printf("Enter n matrix \n");
   scanf("%d", &n);
   int **a = (int*)malloc(n*n * sizeof(int));
@@ -85,13 +86,11 @@ int main()
 
   printf("Matrix a\n");
   show_matrx(matrx1, n);
+  deter = calculateDeterm(matrx1, n);
+  printf("determinant:%d\n", deter);
 
-  int **s = determ_matrx(a, n);
-  printf("determinant:\n");
-  show_matrx(s, n);
-  free(matrx1);
-  free(s);
-
-
-  return 0;
+for (i = 0; i < n; i++){
+    free(matrx1[i]);
+}free(matrx1);
+ return 0;
 }
